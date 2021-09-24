f281d077309a8b4b34baac8ca0e672e8ea925db5
adjprtma <djitothebeat123@gmail.com>
2021-09-24 09:08:30 +0700

    Telah menyelesaikan video ke 6

--- pertemuan3/inheritance-problem.php
+++ pertemuan3/inheritance-problem.php
@@ -3,44 +3,59 @@
 Adji Pratama
 193040101
 https://github.com/adjprtma/prakweb2021_oophp_193040101
-Pertemuan 2 - 17 September 2021
-Praktikum Web
-Mempelajari Object Type
+Pertemuan 3 - 24 September 2021
+Praktikum Web 
+OOP DASAR pada PHP #6 - Inheritance (Bagian 1)
+Mempelajari  Inheritance (Bagian 1)
 */ 
 
 class Produk {
     public $judul,
            $penulis,
            $penerbit,
-           $harga;
+           $harga,
+           $jmlHalaman,
+           $waktuMain,
+           $tipe;
 
-    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe ) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
     }
 
     public function getLabel() {
         return "$this->penulis, $this->penerbit";
     }
-}
 
-
-class CetakInfoProduk {
-    public function cetak( Produk $produk ) {
-        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    public function getInfoLengkap() {
+        // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        if ( $this->tipe == "Komik" ) {
+            $str .= " - {$this->jmlHalaman} Halaman.";
+        } else if ( $this->tipe == "Game" ) {
+            $str .= " ~ {$this->waktuMain} Jam.";
+        }
         return $str;
     }
 }
 
-$produk1 = new Produk("Naruto", "Masahi Kishimoto", "Shonen Jump", 30000);
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 25000);
+    class CetakInfoProduk {
+        public function cetak( Produk $produk ) {
+            $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+            return $str;
+        }
+    }
 
-echo "Komik : " . $produk1->getLabel();
-echo "<br>";
-echo "Game : " . $produk2->getLabel();
-echo "<br>";
-$infoProduk1 = new CetakInfoProduk();
-echo $infoProduk1->cetak($produk1);
+    $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+    $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
+
+    echo $produk1->getInfoLengkap();
+    echo "<br>";
+    echo $produk2->getInfoLengkap();
+       
 ?>
